764bbb9ca17f28c32278787ad5091f0e8693995a
brewchetta <chett.tiller@gmail.com>
2023-08-10 10:43:14 -0400

    replace for with htmlFor

--- src/components/DuckForm.js
+++ src/components/DuckForm.js
@@ -3,10 +3,10 @@ import React from 'react'
 function DuckForm(props) {
   return (
     <form id="new-duck-form">
-       <label for="duck-name-input">New Duck Name:</label>
+       <label htmlFor="duck-name-input">New Duck Name:</label>
        <input type="text" name="duck-name-input" />
 
-       <label for="duck-image-input">New Duck Image URL:</label>
+       <label htmlFor="duck-image-input">New Duck Image URL:</label>
        <input type="text" name="duck-image-input" />
 
        <input type="submit" value="Create Duck" />
